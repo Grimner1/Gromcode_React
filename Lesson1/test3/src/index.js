c794046beb2987e55c287a417f30fb05ffd73e0e
Grimner1 <ursuld@gmail.com>
2021-10-06 11:56:27 +0300

    3-1

--- Lesson1/test3/src/index.js
+++ Lesson1/test3/src/index.js
@@ -25,8 +25,8 @@ const rootElement = document.querySelector('#root');
 
 const element = (
   <div class="greeting">
-    <div class="greeting__title">Hello, world!</div>
-    <div class="greeting__text">I'm learning React</div>
+    <h1 class="greeting__title">Hello, world!</h1>
+    <p class="greeting__text">I'm learning React</p>
   </div>
 );
 
