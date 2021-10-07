9b29848ddb78b97480c2937d1a0a027f99addbaa
Grimner1 <ursuld@gmail.com>
2021-10-07 11:05:31 +0300

    add l1 t5

---
+++ Lesson1/todo-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootDomEl = document.querySelector('#root');
+
+const titleElement = <h1 class="title">Todo List</h1>;
+
+ReactDOM.render(titleElement, rootDomEl);
