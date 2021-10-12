19440df821459febafd848783c723d0f17dc5ae0
Grimner1 <ursuld@gmail.com>
2021-10-12 10:06:07 +0300

    1-4

--- Lesson4/test3/src/App.jsx
+++ Lesson4/test3/src/App.jsx
@@ -7,6 +7,8 @@ const userInfo = {
     'https://ubisoft-avatars.akamaized.net/f49271ad-9ae1-414a-b834-e85ac3c546a3/default_256_256.png',
 };
 
-const App = () => <Comment author={userInfo} text="Good job!" date={'2002'} />;
+const App = () => (
+  <Comment author={userInfo} text="Good job!" date={new Date('2019-01-01T11:32:19.566Z')} />
+);
 
 export default App;
