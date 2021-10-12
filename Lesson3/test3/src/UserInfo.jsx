34031c329b2c1de1d644c30f2348b8663ed416af
Grimner1 <ursuld@gmail.com>
2021-10-12 12:22:07 +0300

    3-7

--- Lesson3/test3/src/UserInfo.jsx
+++ Lesson3/test3/src/UserInfo.jsx
@@ -3,7 +3,7 @@ import Avatar from './Avatar';
 
 const UserInfo = props => (
   <div className="user-info">
-    <Avatar user={props.name} avatarUrl={props.user.avatarUrl} />
+    <Avatar name={props.user.name} avatarUrl={props.user.avatarUrl} />
     <div className="user-info__name">{props.user.name}</div>
   </div>
 );
