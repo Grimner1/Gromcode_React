fa440336c73be571a8e09c6e01c6338256427df5
Grimner1 <ursuld@gmail.com>
2021-10-12 12:53:31 +0300

    add l3 t4

--- Lesson3/test4/src/Greeting.jsx
+++ Lesson3/test4/src/Greeting.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Greeting = (firstName, lastName, birthDate) => {
-  //
+const userAge = date => {
+  const birthDate = new Date(date).getFullYear();
+  const todaysВate = new Date().getFullYear();
+  const yearsOld = todaysВate - birthDate;
+  return yearsOld;
 };
 
+const Greeting = props => {
+  console.log(userAge());
+  return (
+    <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I'm ${userAge(
+      props.birthDate,
+    )} years old`}</div>
+  );
+};
 export default Greeting;
--- Lesson3/test4/src/index.jsx
+++ Lesson3/test4/src/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App.jsx';
+import Greeting from './Greeting';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Greeting firstName="Samyel" lastName="Jeckson" birthDate={new Date('2020-12-30')} />,
+  rootElement,
+);
