3d5f2eee22a746702a4352583ebfa470ac7bacc7
Grimner1 <ursuld@gmail.com>
2021-10-12 11:42:22 +0300

    1-5

--- Lesson4/test3/src/App.jsx
+++ Lesson4/test3/src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import Comment from './Comment';
 
-// const userInfo = {
-//   name: 'Sam',
-//   avatarUrl:
-//     'https://ubisoft-avatars.akamaized.net/f49271ad-9ae1-414a-b834-e85ac3c546a3/default_256_256.png',
-// };
+const userInfo = {
+  name: 'Sam',
+  avatarUrl:
+    'https://ubisoft-avatars.akamaized.net/f49271ad-9ae1-414a-b834-e85ac3c546a3/default_256_256.png',
+};
 
 const App = () => (
-  <Comment
-    //  author={userInfo}
-    text="Good job!"
-    date={new Date('2019-01-01T11:32:19.566Z')}
-  />
+  <Comment author={userInfo} text="Good job!" date={new Date('2019-01-01T11:32:19.566Z')} />
 );
 
 export default App;
--- Lesson4/test3/src/Comment.jsx
+++ Lesson4/test3/src/Comment.jsx
@@ -5,17 +5,17 @@ import UserInfo from './UserInfo';
 
 const formatDate = date => moment(date).format('DD MMM YYYY');
 
-const userInfo = {
-  name: 'Sam',
-  avatarUrl:
-    'https://ubisoft-avatars.akamaized.net/f49271ad-9ae1-414a-b834-e85ac3c546a3/default_256_256.png',
-};
+// const userInfo = {
+//   name: 'Sam',
+//   avatarUrl:
+//     'https://ubisoft-avatars.akamaized.net/f49271ad-9ae1-414a-b834-e85ac3c546a3/default_256_256.png',
+// };
 
 function Comment(props) {
   return (
     <div className="comment">
-      {/* <UserInfo user={props.author} /> */}
-      <UserInfo user={userInfo} />
+      <UserInfo user={props.author} />
+      {/* <UserInfo user={userInfo} /> */}
       <div className="comment__text">{props.text}</div>
       <div className="comment__date">{formatDate(props.date)}</div>
     </div>
