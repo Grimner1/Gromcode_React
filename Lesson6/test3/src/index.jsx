174d10ceca9b3a8eb7e7a2e3c59fd508747e2aca
Grimner1 <ursuld@gmail.com>
2021-10-18 20:49:29 +0300

    add l6 t3

---
+++ Lesson6/test3/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+// import App from './App.jsx';
+import Page from './Page';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Page />, rootElement);
