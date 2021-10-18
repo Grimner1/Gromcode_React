6ea632c9c2ca7835ede6052ea1b01275e2fee64a
Grimner1 <ursuld@gmail.com>
2021-10-18 20:25:31 +0300

    2-2

--- Lesson6/test2/src/index.jsx
+++ Lesson6/test2/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Mailbox from './Mailbpx';
+import Mailbox from './Mailbox';
 
 const rootElement = document.querySelector('#root');
 
