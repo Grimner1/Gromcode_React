f4e1b9d54a949b9e2ad15409ed3007c1e7e47170
Grimner1 <ursuld@gmail.com>
2021-10-18 20:22:13 +0300

    add l6 t2

---
+++ Lesson6/test2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Mailbox from './Mailbpx';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Mailbox unreadMessages={[]} />, rootElement);
