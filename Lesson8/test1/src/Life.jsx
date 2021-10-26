dd8e0fd2216fd5caea1006d7a1294ce1d73ca751
Grimner1 <ursuld@gmail.com>
2021-10-26 15:34:53 +0300

    add l8 t1

---
+++ Lesson8/test1/src/Life.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Life extends Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor: good place to create state');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount:API calls, subscriptions');
+  }
+
+  componentWillUnmount() {
+    console.log('omponentWillUnmount():cleanup before DOM related component will be removed');
+  }
+
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate(nextPrps, nextState): decide to render or not to render');
+    return 1;
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+  }
+
+  render() {
+    console.log('return React element to build DOM');
+    return <div className="number">{this.props.number}</div>;
+  }
+}
+
+export default Life;
+
+// componentDidMount:API calls, subscriptions
+// shouldComponentUpdate(nextPrps, nextState): decide to render or not to render
+// componentDidUpdate(prevProps, prevState): some updates based on new props
+// componentWillUnmount():cleanup before DOM related component will be removed
+// return React element to build DOM
---
+++ Lesson8/test1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+// import App from './App.jsx';
+import Demo from './Demo';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Demo />, rootElement);
