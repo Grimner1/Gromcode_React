7b77207b51bc1e7d271a9f572867c90d79f0d9cf
Grimner1 <ursuld@gmail.com>
2021-10-27 00:09:00 +0300

    add l8 t4

---
+++ Lesson8/test4/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+// import App from './App.jsx';
+import User from './User';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<User userId={'facebook'} />, rootElement);
