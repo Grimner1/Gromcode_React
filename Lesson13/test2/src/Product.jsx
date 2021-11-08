9abac62d6184fa893a14855d2b4da56734563100
Grimner1 <ursuld@gmail.com>
2021-11-08 19:42:44 +0200

    add l13 t2

---
+++ Lesson13/test2/src/Product.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+const Product = ({ ...rest }) => {
+  return <div className="product">ball</div>;
+};
+
+export default Product;
